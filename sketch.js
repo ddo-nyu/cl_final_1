281b15d8624f4f6d7cc4e5e695d88af8a0e2844b
Glitch (glitch-hello-website) <none>
2022-11-30 19:59:27 -0500

    sprite + SoundClassification

--- sketch.js
+++ sketch.js
@@ -5,45 +5,32 @@ let button;
 let classifier;
 let character;
 let floor;
-let hop = 10;
-let isJumping = false;
-let myVideo;
+let hop = 50;
+let myAudio;
+let p5lmDATA;
+let otherVideo;
+let myCanvas;
+let canvasStream
+let p5lm;
 
 function preload() {
-  // Load SpeechCommands18w sound classifier model
   classifier = ml5.soundClassifier("SpeechCommands18w", {
     probabilityThreshold: 0.7,
   });
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  myCanvas = createCanvas(windowWidth, windowHeight);
 
   world.gravity.y = 10;
 
   // create start button
-  button = createButton("click me");
+  button = createButton("Start");
   button.position(0, 0);
-  button.mousePressed(setupModels);
-
-  const x = 250;
-  const y = height;
-
-  character = new Sprite(40, 0, 34);
-  character.addAni('right', 'img/animated/Character_right_0001.png', 6);
-  character.addAni('left', 'img/animated/Character_left_0001.png', 6);
-  character.ani = 'right';
-  character.ani.stop();
+  button.mousePressed(setupLibs);
 
   floor = new Sprite(width / 2, height, width, 200, "static");
   floor.color = "brown";
-}
-
-let otherVideo;
-function gotStream(stream, id) {
-  // otherVideo = stream;
-  //otherVideo.id and id are the same and unique identifier
-  // otherVideo.hide();
 }
 
 function keyPressed() {
@@ -61,41 +48,94 @@ function draw() {
 }
 
 function jump(sprite) {
-  console.log("hop", hop);
-  sprite.move("up", 3, hop).then(() => {
-    isJumping = false;
-  });
+  sprite.move("up", 3, hop);
 }
 
 // helper functions
+function setupLibs() {
+  setupModels();
+  // setupRTC();
+  buildCharacter();
+}
 
-function setupModels() {
+function buildCharacter() {
+  character = new Sprite(40, 0, 34);
+  character.addAni('right', 'img/animated/Character_right_0001.png', 6);
+  character.addAni('left', 'img/animated/Character_left_0001.png', 6);
+  character.ani = 'right';
+  character.ani.stop();
+}
+
+async function setupModels() {
   audioContext = getAudioContext();
   mic = new p5.AudioIn();
   // mic.start(startPitch);
-  classifier.classify(gotResult);
-
-  let constraints = { audio: true, video: false };
-  myVideo = createCapture(constraints, function (stream) {
-    let p5lm = new p5LiveMedia(this, "CAPTURE", stream, "jZQ64AMJc");
-    p5lm.on("stream", gotStream);
-  });
-  myVideo.elt.muted = true;
-  myVideo.hide();
-
-  character.ani.play();
+  classifier.classify(parseCommand);
 }
 
-function gotResult(error, results) {
+function setupRTC() {
+  // Use constraints to request audio from createCapture
+  let constraints = {
+    audio: true
+  };
+
+  // Need to use the callback to get at the audio/video stream
+  myAudio = createCapture(constraints, async function(stream) {
+    console.log(stream)
+    character.ani.play();
+
+    // Get a stream from the canvas to send
+    canvasStream = myCanvas.elt.captureStream(15);
+
+    // Extract the audio tracks from the stream
+    let audioTracks = stream.getAudioTracks();
+
+    // Use the first audio track, add it to the canvas stream
+    if (audioTracks.length > 0) {
+      canvasStream.addTrack(audioTracks[0]);
+      // pitch = ml5.pitchDetection("./model/", audioContext, canvasStream, modelLoaded);
+
+
+      const mediaRecorder = new MediaRecorder(canvasStream);
+      const recognizer = speechCommands.create('BROWSER_FFT');
+      await recognizer.ensureModelLoaded();
+      const modelInputShape = await recognizer.modelInputShape();
+      console.log('modelInputShape', modelInputShape)
+      console.log(recognizer.params().sampleRateHz);
+      console.log(recognizer.params().fftSize);
+      const x = tf.tensor4d(mediaRecorder, [1].concat(recognizer.modelInputShape().slice(1)));
+      const output = await recognizer.recognize(x);
+      // output has the same format as `result` in the online streaming example
+      // above: the `scores` field contains the probabilities of the words.
+
+      tf.dispose([x, output]);
+    }
+
+    // Give the canvas stream to SimpleSimplePeer as a "CAPTURE" stream
+    p5lm = new p5LiveMedia(this, "CAPTURE", canvasStream, "SimpleSimplePeerAdvancedTest");
+    p5lm.on('stream', gotStream);
+    p5lm.on('data', parseData);
+  });
+  myAudio.elt.muted = true;
+  myAudio.hide();
+}
+
+function gotStream(stream) {
+  otherVideo = stream;
+  otherVideo.hide();
+}
+
+function parseData(data, id) {
+  console.log(JSON.parse(data))
+}
+
+function parseCommand(error, results) {
   if (error) {
     console.error(error);
   }
   switch (results[0].label) {
     case "up":
-      if (!isJumping) {
-        isJumping = true;
-        jump(character);
-      }
+      jump(character);
       break;
     case "right":
       character.ani = "right";
@@ -108,24 +148,16 @@ function gotResult(error, results) {
   }
 }
 
-function startPitch() {
-  pitch = ml5.pitchDetection("./model/", audioContext, mic.stream, modelLoaded);
-}
-
 function modelLoaded() {
-  console.log("Model Loaded");
+  console.log("PitchDetection Model Loaded");
   getPitch();
 }
 
 function getPitch() {
   pitch.getPitch(function (err, frequency) {
-    // console.log(frequency);
     if (frequency) {
-      // console.log(frequency);
-      if (frequency > 200) {
-        // console.log(frequency);
-        hop = frequency;
-      }
+      // console.log('frequency', frequency);
+      p5lm.send(JSON.stringify({ frequency }));
     }
     getPitch();
   });
